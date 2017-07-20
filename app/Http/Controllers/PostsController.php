a7b3b30dddce5afea8ab18e72d1675efd12be6c6
Aleksa <aleksadoderovic@gmail.com>
2017-07-20 09:54:18 +0200

    load posts from db and show by clicking title

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -8,7 +8,11 @@ use App\Post;
 class PostsController extends Controller
 {
     public function index(){
-      return view('posts.index');
+      $posts = Post::latest()->get();
+      return view('posts.index', compact('posts'));
+    }
+    public function show(Post $post){
+      return view('posts.show', compact('post'));
     }
     public function create(){
       return view('posts.create');
