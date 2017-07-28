59b809775d5c51505ca9e2376836270c256b21dc
Aleksa <aleksadoderovic@gmail.com>
2017-07-28 16:44:44 +0200

    add comment section/model/controller

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -29,4 +29,5 @@ class PostsController extends Controller
 
       return redirect('/');
     }
+    
 }
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -3,5 +3,34 @@
   <div class="col-sm-8 blog-main">
     <h1>{{$post->title}}</h1>
     <p>{{$post->body}}</p>
+
+  <hr>
+  <div class="comments">
+    <ul class="list-group">
+      @foreach($post->comments as $comment)
+      <li class="list-group-item">
+        <strong>{{$comment->created_at->diffForHumans()}}</strong><br>
+        {{$comment->body}}
+      </li>
+      @endforeach
+    </ul>
+
+  </div>
+  <hr>
+  <div class="card">
+    <div class="card-block">
+      <form method="post" action="/posts/{{$post->id}}/comments">
+        {{ csrf_field() }}
+        <div class="form-group">
+          <textarea name="body" placeholder="Your comment here" class="form-control" required></textarea>
+        </div>
+        <div class="form-group">
+          <button type="submit" class="btn btn-primary">Add comment</button>
+        </div>
+      </form>
+      @include('layouts.errors')
+    </div>
+
+  </div>
   </div>
 @endsection
